2e185bfd1b5ba23484fe2c63a3b0dc575d8fff39
hyunseoKoo <koohs414@naver.com>
2023-07-24 15:45:59 +0900

    feat: 데이터 통신 & 상태관리 설정

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import ThumbCard from '../components/ThumbCard';
+import Youtube from '../api/youtube';
 
 export default function Videos() {
     const {keyword} = useParams();
     const {isLoading, error, data: videos} = useQuery(
-        ['videos', keyword],
-        async () => {
-            return fetch(`/data/${keyword? 'keyword' : 'popular'}.json`)
-            .then(res => res.json())
-            .then((data) => data.items)
+        ['videos', keyword], () => {
+            const youtube = new Youtube();
+            return youtube.search(keyword);
         }
     );
 
